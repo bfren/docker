46bdf550141abea904133f7f0815b09a6b86a57c
Ben Green <ben@bcgdesign.com>
2022-03-16 08:09:57 +0000

    Adding additional cleanup

--- update.sh
+++ update.sh
@@ -10,9 +10,16 @@ for DIR in `ls`; do
   echo "${DIR} .. pull"
   git pull
 
-  echo "${DIR} .. clean"
-  BRANCHES=`git branch -r | grep "origin/v" | sed 's/origin\///'`
-  [[ -n "${BRANCHES}" ]] && git branch -D ${BRANCHES}
+  echo "${DIR} .. prune"
+  git remote prune origin
+
+  echo "${DIR} .. delete merged branches"
+  MERGED=`git branch -r | grep "origin/v" | sed 's/origin\///'`
+  [[ -n "${MERGED}" ]] && git branch -D ${MERGED}
+
+  echo "${DIR} .. delete not merged branches"
+  NOT_MERGED=`git branch --no-merged`
+  [[ -n "${NOT_MERGED}" ]] && git branch -D ${NOT_MERGED}
 
   cd ..
 
