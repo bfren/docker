fa74d0a14c9c68ac396e9ef37c49be180d259c23
bfren <bf@bfren.dev>
2025-11-17 13:41:25 +0000

    Use array to pull and push golang images

--- golang.sh
+++ golang.sh
@@ -1,7 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-docker pull --platform linux/arm/v7 golang:alpine
-docker pull --platform linux/arm64 golang:alpine
-docker pull --platform linux/amd64 golang:alpine
+PLATFORMS=(linux/arm/v7 linux/arm64 linux/amd64)
+
+for P in ${PLATFORMS[@]}; do
+    docker pull --platform ${P} golang:alpine
+done
+
 docker tag golang:alpine ghcr.io/bfren/golang:alpine
-docker push ghcr.io/bfren/golang:alpine
+
+for P in ${PLATFORMS[@]}; do
+    docker push --platform ${P} ghcr.io/bfren/golang:alpine
+done
